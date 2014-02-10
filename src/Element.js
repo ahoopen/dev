15fee2a8480b81ef7a5689531b558c5766029c82
auke ten hoopen <a.tenhoopen@gmail.com>
2014-02-10 16:56:48 +0100

    aanpassingen aan de resource classe

--- src/Element.js
+++ src/Element.js
@@ -1,4 +1,4 @@
-define(["Application"], function(app) {
+define("element", function() {
 
 	var $element = function() {
 
@@ -100,7 +100,134 @@ define(["Application"], function(app) {
 		**/
 		isBoolean : function(value) {
 			return typeof value === 'boolean';
+		},
+
+		/**
+		* Bepaalt het aantal elementen in een array, het aantal properties van een object of
+		* de lengte van de string.
+		*
+		* @name $element.size
+		* @function
+		* @param {Object|Array|string} obj Object, array, of string om te doorzoeken.
+		* @returns {number} de grootte van het 'obj'
+		**/
+		size : function(obj, ownPropsOnly) {
+			var count = 0, key;
+
+			if ( this.isArray(obj) || this.isString(obj)) {
+				return obj.length;
+			} else if (this.isObject(obj)) {
+				for (key in obj) {
+					if (!ownPropsOnly || obj.hasOwnProperty(key)) {
+						count++;
+					}
+				}
+			}
+
+			return count;
+		},
+
+		/**
+		* Geeft aan of het object leeg is.
+		*
+		* @name $element.isEmpty
+		* @function
+		* @param {Object} obj Object om te kijken
+		* @returns {boolean} true als object leeg is
+		**/
+		isEmpty : function(obj) {
+			var has = Object.prototype.hasOwnProperty;
+
+			// null en undefined zijn "empty"
+			if(obj == null) return true;
+
+
+			if(obj.length && obj.length > 0) return false;
+			if(obj.length === 0) return true;
+
+			for( var key in obj) {
+				if( has.call(obj, key)) return false;
+			}
+
+			return true;
+		},
+
+		/**
+		* Geeft aan of het object in de array zit
+		*
+		* @name $element.contains
+		* @function
+		* @param {Object} obj Object om te kijken
+		* @returns {int} de positie van de gevonden object in de array 
+		**/
+		contains : function(array, obj) {
+			return this.indexOf(array, obj);
+		},
+
+		indexOf : function(array, obj) {
+			if( array.indexOf ) return array.indexOf(obj);
+
+			for (var i = 0; i < array.length; i++) {
+				if (obj === array[i]) return i;
+			}
+			return -1;
+		},
+
+		isArrayLike : function( obj ) {
+		  if (!obj || (typeof obj.length !== 'number')) return false;
+
+		  // We have on object which has length property. Should we treat it as array?
+		  if (typeof obj.hasOwnProperty != 'function' &&
+		      typeof obj.constructor != 'function') {
+		    // This is here for IE8: it is a bogus object treat it as array;
+		    return true;
+		  } else {
+		    return (jQuery && obj instanceof jQuery) || // jQuery
+		           typeof obj.callee === 'function'; // arguments (on IE8 looks like regular obj)
+		  }
+		  // toString.call(obj) !== '[object Object]' || // some browser native object
+		},
+
+		/**
+		* Roept de iterator functie aan voor elke element in 'obj'. De iterator functie word aangeroepen als 'iterator( value, key )'
+		* waar value de waarde is van object propertie of een element van een array en de 'key' is de object key of array index. Een context defineren
+		* is optioneel.
+		*
+		* @name $element.forEach
+		* @function
+	 	* @param {Object|Array} obj Object to iterate over.
+		* @param {Function} iterator Iterator function.
+		* @param {Object} context Object to become context (`this`) for the iterator function.
+		* @returns {number} de grootte van het 'obj'
+		**/
+		forEach : function( obj, iterator, context ) {
+			var key;
+
+			if ( obj ) {
+				if ( this.isFunction( obj) ) {
+					for( key in obj ) {
+						if ( key != 'prototype' && key != 'length' && key != 'name' && obj.hasOwnProperty(key) ) {
+							iterator.call(context, obj[key], key);
+						}
+					}
+				} else if ( obj.forEach && obj.forEach !== this.forEach ) {
+					obj.forEach(iterator, context);
+				} else if ( this.isArrayLike( obj ) ) {
+					for( key = 0; key < obj.length; key++ ) {
+						iterator.call(context, obj[key], key);
+					}
+				} else {
+					for ( key in obj ) {
+						if (obj.hasOwnProperty(key) ) {
+							iterator.call(context, obj[key], key);
+						}
+					}
+				}
+			}
+
+			return obj;
 		}
 	};
 
+	return $element;
 });
--- src/Resource.js
+++ src/Resource.js
@@ -1,7 +1,8 @@
-define(["Application"], function(app) {
+define(["Element"], function($element) {
 	
-	var $resource = function() {
-		this.$element = null;
+	
+	var $resource = function(Element) {
+		
 	};
 
 	$resource.prototype = {
@@ -15,15 +16,92 @@ define(["Application"], function(app) {
 		* @returns {boolean} true wanneer 'value' undefined is
 		**/
 		image : function(src, options) {
+			var img = $(src);
 
-			//
-			forEach(options, function(option) {
-				if( this.$element.isObject(option) ) {
-					
-				} else if( this.$element.isFunction(option) ) {
+			//if(img) {
+			//	src = options.src || img.src;
+			//} else {
+				img = document.createElement('img');
+				src = src || options.src;
+			//}
 
+			if (!src) {
+				return null;
+			}
+
+			img = this.setImageProperties(img, options);
+
+			var prop = this.$element.isUndefined(img.naturalWidth) ? 'width' : 'naturalWidth';
+			img.src = src;
+
+		    // Loaded?
+		    if (img.complete) {
+		        if (img[prop]) {
+		            if ( this.$element.isFunction(options.success) ) {
+		                options.success.call(img);
+		            }
+		        } else {
+		            if ( this.$element.isFunction(options.failure) ) {
+		                options.failure.call(img);
+		            }
+		        }
+		    } else {
+		        if ( this.$element.isFunction(options.success) ) {
+		            img.onload = options.success;
+		        }
+		        if ( this.$element.isFunction(options.failure) ) {
+		            img.onerror = options.failure;
+		        }
+		    }
+
+		    return img;
+		},
+
+		/**
+		* zet de afbeelding properties.
+		*
+		* @name $resource.setImageProperties
+		* @function
+		* @param {Object} img image object
+		* @param {Array} properties array met afbeelding eigenschappen.
+		* @returns {Object} image object
+		**/
+		setImageProperties : function(img, properties) {
+			var attributes = ['align', 'alt', 'border', 'height', 'name', 'src', 'width', 'longDesc', 'id', 'class'],
+				self = this;
+
+			this.$element.forEach( properties, function(property) {
+				var attribute = self.$element.contains(attributes, property);
+				if ( attribute ) {
+					img[attribute] = property;
 				}
 			});
+
+			return img;
+		},
+
+		imageComplete : function() {
+
+		},
+
+		/**
+		* Behoudt de aspect ratio van het orgineel. Handig bij het verkleinen/vergroten
+		* van een afbeeldingen om ze passend te maken voor een bepaald gebied.
+		*
+		* @param {Number} srcWidth Source area width
+		* @param {Number} srcHeight Source area height
+		* @param {Number} maxWidth Fittable area maximum available width
+		* @param {Number} maxHeight Fittable area maximum available height
+		* @return {Object} { width, heigth }
+		*
+		*/
+		calculateAspectRatioFit : function(srcWidth, srcHeight, maxWidth, maxHeight) {
+			var ratio = [maxWidth / srcWidth, maxHeight / srcHeight ];
+			ratio = Math.min(ratio[0],ratio[1]);
+
+			return { width : srcWidth*ratio, height : srcHeight*ratio };
 		}
 	};
+
+	return $resource;
 });
